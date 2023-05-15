e5450e63d261cf5d77183f2956601473eaf4fd5c
Geethasri48 <84894714+Geethasri48@users.noreply.github.com>
2023-05-15 11:27:35 +0530

    Permutation1.cpp

---
+++ Permutation1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a[n],b[n];
+    cin >>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        b[a[i]-1]=i;
+    }
+    int l=b[n-1];
+    int r=b[n-1];
+    int res=0;
+    for(int i=1;i<=n;i++)
+    {
+        int ind=b[n-i];
+        if(ind<l)l=ind;
+        else if(ind>r)r=ind;
+        if(r-l+1==i)res=i;
+    }
+    cout << res<<"\n";
+    cout << l<< r;
+}
