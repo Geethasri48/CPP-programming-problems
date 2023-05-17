589346e7e7b2b1d55b6626561a0665bc1637950e
Geethasri48 <84894714+Geethasri48@users.noreply.github.com>
2023-05-17 11:17:56 +0530

    Segmented_Sieve.cpp

--- Segmented_Sieve.cpp
+++ Segmented_Sieve.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 #include<stdio.h>
-using namespacestd;
+#include<iostream>
+using namespace std;
 typedef long long ll;
 #define N 1e6
 vector<int>v(N,true);
 void prime(){
     v[0]=v[1]=false;
-    for(int i=2;i*i<=R;i++)
+    for(int i=2;i*i<=N;i++)
     {
         if(v[i]==true)
         {
@@ -18,24 +19,24 @@ void prime(){
     }
 }
 vector<int>seg_siv(int n){
-    vector<int>primes();
+    vector<int>primes;
     for(int i=2;i<=n;i++)
     {
         if(v[i]==true)primes.push_back(i);
     }
+    return primes;
 }
-
 int main()
 {
     prime();
     int l,r;
     cin>>l >>r;
-    vector<int>res=seg_siv(sqrt(n));
+    vector<int>res=seg_siv(sqrt(r));
     vector<int>res1(r-l+1,1);
     for(auto it:res)
     {
         int fm=(l/it)*it;//to get multiples
-        fm=(fm<l)?fm+l:fm;
+        fm=(fm<l)?fm+it:fm;
         for(int i=max(fm,it*it);i<=r;i+=it)
         {
             res1[i-l]=0;
@@ -43,6 +44,9 @@ int main()
     }
     for(int i=0;i<res1.size();i++)
     {
-        cout << res1[i] <<"\n";
+        if(res1[i]==1)
+        {
+            cout << i+l<<"\n";
+        }
     }
 }
